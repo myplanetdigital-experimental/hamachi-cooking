7c2fbe3d13324b05ca2db863f019841fb0ace852
Patrick Connolly <patrick@myplanetdigital.com>
2012-01-09 11:08:59 -0500

    Generalized knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,4 @@
-cookbook_path          '/Users/homebase/vagrant/experimentation/logmein-hamachi-cooking/cookbooks'
+require 'librarian/chef/integration/knife'
+current_dir = File.dirname(__FILE__)
+
+cookbook_path = [ Librarian::Chef.install_path, "{current_dir}/../cookbooks-in-progress" ]
